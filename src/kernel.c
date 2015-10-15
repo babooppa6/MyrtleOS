c35dd04c07cbadfc28be77eb19d99fa81e9b4317
Finn Thomas <pitechgames@gmail.com>
2015-10-15 19:17:36 +0100

    Made a DOS type thingy (temporary)

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,22 @@
-#include "include/print.h"
+#include "include/screen.h"
+#include "include/keyboard.h"
+#include "include/string.h"
 
 kmain() {
-  print("Hello, World");
+	clearScreen();
+	for(;;) {
+		print("MyrtleDOS> ");
+		string command = readString();
+		switch (command) {
+			case "info":
+				print("MyrtleOS is an open-source OS, made in C with a sprinkle of Assembly.\n Look at my insides at github.com/finnthomas/MyrtleOS");
+				break;
+			case "cls":
+				clearScreen();
+				break;
+			default:
+				print("Sorry - I don't know what that means :-(");
+				break;
+		}
+	}
 }
