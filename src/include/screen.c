2eb279b158a15272ad9c21137e712a67d3c16ec9
Finn Thomas <pitechgames@gmail.com>
2015-10-15 19:37:47 +0100

    Added printch function

--- src/include/screen.c
+++ src/include/screen.c
@@ -45,3 +45,19 @@ void print(string str, char colour) {
 		}
 	}
 }
+
+void printch(char ch) {
+	char* vidmem[] = (char*)0xB8000; // access video memory 
+	int pos = (y * 2) + x; // Get position
+	vidmem[pos]   = *str; // Print char
+	vidmem[pos++] = 0x07; // Colour
+	if (*str == '\n') { // If newline
+		y++;
+		x = 0;
+	} else if (*str == '\t'){ // If tab
+		x += 10;
+	} else {
+		x += 2;
+	}
+}
+
