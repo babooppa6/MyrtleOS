9116879c87a4109310ff0bfb234654cb48960c7d
Finn Thomas <pitechgames@gmail.com>
2015-10-15 19:02:54 +0100

    Added print function, getting rid of print.h and print.c
    I thought it would be more logical :)

--- src/include/screen.h
+++ src/include/screen.h
@@ -1,6 +1,9 @@
 #ifndef SCREEN_H
 #define SCREEN_H
+#include "types.h"
 const uint8 sw = 80, sh = 25, sd = 2; // Screen dimensions
 void clearLine(uint8 from, uint8 to);
 void clearScreen();
+void print(string str);
+void print(string str, char colour);
 #endif
